83e62a46fe27eab40adde50bb0d3af023deec81c
yongsk0066 <yongsk0066@naver.com>
2023-09-24 12:56:07 +0900

    🚀 github action 세팅

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
 import lit from "@astrojs/lit";
-
 import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [lit(), mdx()]
+  integrations: [lit(), mdx()],
+  site: 'https://yongsk0066.github.io',
+  base: '/'
 });
