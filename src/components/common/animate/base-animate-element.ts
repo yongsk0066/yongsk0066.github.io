685881cbb0964398c97dca135971c12a868e0b06
yongsk0066 <yongsk0066@naver.com>
2023-10-03 16:12:21 +0900

    refactor: BaseAnimateElement 리팩토링(체이닝방식 initaizlize)

--- src/components/common/animate/base-animate-element.ts
+++ src/components/common/animate/base-animate-element.ts
@@ -1,72 +1,147 @@
 // BaseAnimateElement.ts
-import { LitElement } from 'lit';
+import { LitElement, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import * as THREE from 'three';
-import type { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPixelatedPass } from 'three/addons/postprocessing/RenderPixelatedPass.js';
 
+interface BaseAnimateOptions {
+  showAxisHelper?: boolean;
+}
 
 @customElement('base-animate-element')
 export class BaseAnimateElement extends LitElement {
+  static styles = css`
+    canvas {
+      display: block;
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  `;
+
   protected scene?: THREE.Scene;
   protected camera?: THREE.PerspectiveCamera;
   protected clock: THREE.Clock = new THREE.Clock();
   protected renderer?: THREE.WebGLRenderer;
   protected composer?: EffectComposer;
+  protected options: BaseAnimateOptions;
 
-  protected getContainer(): HTMLElement | null {
-    return this.shadowRoot?.querySelector('div');
+  private boundHandleResize: any;
+
+  constructor(options: BaseAnimateOptions = {}) {
+    super();
+    this.options = this.validateOptions(options);
+    this.boundHandleResize = this.handleResize.bind(this);  // bind once and keep the reference
   }
 
-  protected initScene(): boolean {
+  private validateOptions(options: BaseAnimateOptions): BaseAnimateOptions {
+    return options;
+  }
+
+  protected beforeInitialize(): this {
+    return this;
+  }
+
+  protected afterInitialize(): this {
+    return this;
+  }
+
+  protected initialize(){
+    this.beforeInitialize();
+    this.initScene()
+        .initCamera()
+        .initRenderer()
+        .initComposer()
+        .initLighting();
+    this.afterInitialize();
+    return this
+  }
+
+  public firstUpdated() {
+    this.initialize();
+    window.addEventListener('resize', this.boundHandleResize);  // Use the bound function
+  }
+
+
+
+  protected getContainer(){
+    return this.shadowRoot?.querySelector('[data-container="3d-scene"]');
+  }
+
+
+  protected initScene() {
     this.scene = new THREE.Scene();
-    return true;
+    if (this.options.showAxisHelper) {
+      const axisHelper = new THREE.AxesHelper(5);
+      this.scene.add(axisHelper);
+    }
+    return this;
   }
 
-  protected initCamera(): boolean {
+  protected initCamera() {
     const container = this.getContainer();
     const width = container?.clientWidth || 0;
     const height = container?.clientHeight || 0;
     this.camera = new THREE.PerspectiveCamera(16, width / height, 0.1, 1000);
-    return true;
+    return this;
   }
 
-  protected initRenderer(): boolean {
+  protected initRenderer() {
     const container = this.getContainer();
     const width = container?.clientWidth || 0;
     const height = container?.clientHeight || 0;
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(width, height);
     container?.appendChild(this.renderer.domElement);
-    return true;
+    return this;
   }
 
-  protected handleResize(): void {
-    const container = this.getContainer();
-    if (!container || !this.renderer || !this.camera) return;
-    const width = container.clientWidth;
-    const height = container.clientHeight;
-    this.camera.aspect = width / height;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(width, height);
+  protected initComposer() {
+    if (!this.scene || !this.clock || !this.camera || !this.renderer) return this;
+    this.composer = new EffectComposer(this.renderer);
+    const renderPixelatedPass = new RenderPixelatedPass(16, this.scene, this.camera,{
+      normalEdgeStrength: 1,
+      depthEdgeStrength: 1,
+    });
+    this.composer.addPass(renderPixelatedPass);
+    return this;
   }
 
-  firstUpdated(): void {
-    if (!this.initScene() || !this.initCamera() || !this.initRenderer()) return;
-    this.addCustomObject();
-    this.animateScene();
-    window.addEventListener('resize', this.handleResize.bind(this));
-  }
-
-  protected addCustomObject(): void {
-    // To be overridden by subclasses
-  }
-
-  protected animateScene(): void {
-    // To be overridden by subclasses
+  protected initLighting() {
+    if (!this.scene) return this;
+    this.scene.add(new THREE.AmbientLight(0x757f8e, 3));
+    const directionalLight = new THREE.DirectionalLight(0xfffecd, 1.5);
+    directionalLight.position.set(100, 100, 100);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.set(2048, 2048);
+    this.scene.add(directionalLight);
+    const spotLight = new THREE.SpotLight(0xffc100, 10, 10, Math.PI / 16, .02, 2);
+    spotLight.position.set(2, 2, 0);
+    const target = spotLight.target;
+    this.scene.add(target);
+    target.position.set(0, 0, 0);
+    spotLight.castShadow = true;
+    this.scene.add(spotLight);
+    return this;
   }
 
   disconnectedCallback(): void {
-    window.removeEventListener('resize', this.handleResize.bind(this));
+    window.removeEventListener('resize', this.boundHandleResize);  // Use the bound function
     super.disconnectedCallback();
+  }
+
+  protected handleResize() {
+    const container = this.shadowRoot?.querySelector('div');
+    if (!container || !this.renderer || !this.camera || !this.composer) return;
+  
+    const width = container.clientWidth;
+    const height = container.clientHeight;
+  
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+  
+    this.renderer.setSize(width, height);
+    this.composer.setSize(width, height);
   }
 }
--- src/components/thumbnail-element.ts
+++ src/components/thumbnail-element.ts
@@ -1,11 +1,12 @@
-import { LitElement, css, html } from 'lit';
+import { css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import * as THREE from 'three';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { RenderPixelatedPass } from 'three/addons/postprocessing/RenderPixelatedPass.js';
+import { BaseAnimateElement } from './common/animate/base-animate-element';
+
+
 
 @customElement('thumbnail-element')
-export class ThumbnailElement extends LitElement {
+export class ThumbnailElement extends BaseAnimateElement {
 
   static styles = css`
     div {
@@ -15,125 +16,31 @@ export class ThumbnailElement extends LitElement {
       border-radius: 16px;
       overflow: hidden;
     }
-
-    canvas {
-      display: block;
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-
-    }
   `;
 
-  scene?: THREE.Scene;
-  camera?: THREE.PerspectiveCamera
-  clock?: THREE.Clock;
-  renderer?: THREE.WebGLRenderer;
-  composer?: EffectComposer;
   parentObject?: THREE.Object3D;
 
-  
   constructor() {
-    super();
-    this.clock = new THREE.Clock();
+    super({
+      showAxisHelper:true,
+    });
     this.animateScene = this.animateScene.bind(this);
-  }
-  getContainer(){
-    return this.shadowRoot?.querySelector('div');
   }
 
   firstUpdated() {
-    if(!this.initialize()) return;
+    super.firstUpdated();
     this.addObject();
     this.animateScene();
-    window.addEventListener('resize', this.handleResize.bind(this)); // Register resize event listener
   }
 
   disconnectedCallback(): void {
     window.removeEventListener('resize', this.handleResize.bind(this));
   }
 
-  handleResize() {
-    const container = this.shadowRoot?.querySelector('div');
-    if (!container || !this.renderer || !this.camera || !this.composer) return;
-  
-    const width = container.clientWidth;
-    const height = container.clientHeight;
-  
-    this.camera.aspect = width / height;
-    this.camera.updateProjectionMatrix();
-  
-    this.renderer.setSize(width, height);
-    this.composer.setSize(width, height);
-  }
-
-  initialize(){
-    return this.initScene() && this.initCamera() && this.initRenderer() && this.initComposer() && this.initLighting();
-  }
-
-
-  initScene() {
-    this.scene = new THREE.Scene();
+  addObject() {
+    if (!this.scene || !this.clock || !this.camera || !this.renderer) return;
     this.parentObject = new THREE.Object3D();
     this.scene.add(this.parentObject);
-
-    // add objects to scene (Helper)
-    const axisHelper = new THREE.AxesHelper( 5 );
-    this.scene.add( axisHelper );
-    return true;
-  }
-
-  initCamera() {
-    const container = this.getContainer();
-    const width = container?.clientWidth || 0;
-    const height = container?.clientHeight || 0;
-    this.camera = new THREE.PerspectiveCamera(16, width / height, 0.1, 1000);
-    return true;
-  }
-
-  initRenderer() {
-    const container = this.getContainer();
-    const width = container?.clientWidth || 0;
-    const height = container?.clientHeight || 0;
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(width, height);
-    container?.appendChild(this.renderer.domElement);
-    return true;
-  }
-
-  initComposer() {
-    if (!this.scene || !this.clock || !this.camera || !this.renderer) return;
-    this.composer = new EffectComposer(this.renderer);
-    const renderPixelatedPass = new RenderPixelatedPass(16, this.scene, this.camera,{
-      normalEdgeStrength: 1,
-      depthEdgeStrength: 1,
-    });
-    this.composer.addPass(renderPixelatedPass);
-    return true;
-  }
-
-  initLighting() {
-    if (!this.scene) return;
-    this.scene.add(new THREE.AmbientLight(0x757f8e, 3));
-    const directionalLight = new THREE.DirectionalLight(0xfffecd, 1.5);
-    directionalLight.position.set(100, 100, 100);
-    directionalLight.castShadow = true;
-    directionalLight.shadow.mapSize.set(2048, 2048);
-    this.scene.add(directionalLight);
-    const spotLight = new THREE.SpotLight(0xffc100, 10, 10, Math.PI / 16, .02, 2);
-    spotLight.position.set(2, 2, 0);
-    const target = spotLight.target;
-    this.scene.add(target);
-    target.position.set(0, 0, 0);
-    spotLight.castShadow = true;
-    this.scene.add(spotLight);
-    return true;
-  }
-
-
-  addObject() {
-    if (!this.scene || !this.clock || !this.camera || !this.renderer || !this.parentObject) return;
-
     this.parentObject.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 6);
     
     // add objects to scene (Child Object)
@@ -160,6 +67,7 @@ export class ThumbnailElement extends LitElement {
   }
 
   render() {
-    return html`<div></div>`;
+    return html`
+    <div data-container="3d-scene"></div>`;
   }
 }
