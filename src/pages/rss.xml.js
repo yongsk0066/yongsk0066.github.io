592387bd09118ab21010fc9fb33fd2a21058da34
yongsk0066 <yongsk0066@naver.com>
2023-10-14 21:37:56 +0900

    feat: rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+
+  return rss({
+    title: 'YONGSEOK’s Blog',
+    description: 'YONGSEOK’s Blog description',
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      customData: post.data.customData,
+      link: `/post/${post.slug}/`,
+    })),
+    customData: `<language>ko-KR</language>`,
+  });
+}
