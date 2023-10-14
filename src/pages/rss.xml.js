fb4d9f1a164afbcb6303e3e53d4bfdc3cb9fd435
yongsk0066 <yongsk0066@naver.com>
2023-10-14 21:45:38 +0900

    fix: rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,20 +1,12 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:post';
+
 
 export async function GET(context) {
-  const posts = await getCollection('');
-
   return rss({
     title: 'YONGSEOK’s Blog',
     description: 'YONGSEOK’s Blog description',
     site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      customData: post.data.customData,
-      link: `/post/${post.slug}/`,
-    })),
+    items: [],
     customData: `<language>ko-KR</language>`,
   });
 }
