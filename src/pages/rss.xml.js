827309c262c3b5c6eaaa880a9c2a06c9452c5674
yongsk0066 <yongsk0066@naver.com>
2024-05-03 23:27:35 +0900

    fix: rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { SITE_DESCRIPTION, SITE_TITLE } from '@consts';
 
 
 
@@ -15,6 +15,7 @@ export async function GET(context) {
 		items: posts.map((post) => {
 			return ({
 			...post.data,
+			pubDate: post.data.date,
 			link: `/blog/${post.slug}/`,
 			})
 		})
