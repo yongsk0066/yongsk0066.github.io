0666f630b499d74b05585352cf8b8ba9943aa2ff
yongsk0066 <yongsk0066@naver.com>
2024-06-01 14:55:42 +0900

    feat: add work education contact

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,18 @@
+import type { Socials } from "@types";
+
 export const SITE_TITLE = "장용석 블로그";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SOCIALS: Socials = [
+  {
+    NAME: "twitter-x",
+    HREF: "https://x.com/yongsk0066",
+  },
+  {
+    NAME: "github",
+    HREF: "https://github.com/yongsk0066",
+  },
+  {
+    NAME: "linkedin",
+    HREF: "https://www.linkedin.com/in/%EC%9A%A9%EC%84%9D-%EC%9E%A5-129267181/",
+  },
+];
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type Socials = {
+  NAME: string;
+  HREF: string;
+}[];
