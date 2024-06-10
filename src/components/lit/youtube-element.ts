e94e33b2c6bc7fc57d51c5ea3fe6ec09f008f390
yongsk0066 <yongsk0066@naver.com>
2024-06-11 00:13:07 +0900

    React compiler 3

--- src/components/lit/youtube-element.ts
+++ src/components/lit/youtube-element.ts
@@ -4,7 +4,9 @@ import { FigureElement } from "./common/figure-element";
 @customElement("youtube-element")
 export class YouTube extends FigureElement {
   get iframeSrc() {
-    return `https://www.youtube.com/embed/${this.extractVideoId(this.src)}`;
+    const videoId = this.extractVideoId(this.src);
+    const startTime = this.extractStartTime(this.src);
+    return `https://www.youtube.com/embed/${videoId}?start=${startTime}`;
   }
 
   get iframeAttributes(): Partial<HTMLIFrameElement> {
@@ -22,5 +24,10 @@ export class YouTube extends FigureElement {
     const regExp = /^https:\/\/youtu\.be\/([a-zA-Z0-9_-]+)/;
     const match = url.match(regExp);
     return match ? match[1] : "";
+  }
+  extractStartTime(url?: string) {
+    if (!url) return "0";
+    const urlObj = new URL(url);
+    return urlObj.searchParams.get("t") || "0";
   }
 }
