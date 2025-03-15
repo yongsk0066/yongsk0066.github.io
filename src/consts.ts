f3add6347bcc86794d2f9fd1bcf3696913ce0d6b
yongsk0066 <yongsk0066@naver.com>
2025-03-15 17:58:08 +0900

    add: og image

--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,10 @@ export const OTHER_LOCALES = ["en"];
 
 export const SITE_TITLE = "장용석 블로그";
 export const SITE_TITLE_EN = "Yongseok's Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "장용석의 개발, 기술, 그리고 일상블로그 입니다.";
+export const SITE_DESCRIPTION_EN =
+  "A blog about Yongseok's development, technology, and daily life.";
 export const SOCIALS: Socials = [
   {
     NAME: "twitter-x",
