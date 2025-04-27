db22089ab612c873a3581101e1597d975f9222c3
yongsk0066 <yongsk0066@naver.com>
2025-04-27 16:11:15 +0900

    update: about

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,32 +9,57 @@ export function formatDate(date: Date) {
   return Intl.DateTimeFormat("en-US", {
     month: "short",
     day: "2-digit",
-    year: "numeric"
+    year: "numeric",
   }).format(date);
 }
 
 export function readingTime(html: string) {
   const textOnly = html.replace(/<[^>]+>/g, "");
   const wordCount = textOnly.split(/\s+/).length;
-  const readingTimeMinutes = ((wordCount / 200) + 1).toFixed();
+  const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
   return `${readingTimeMinutes} min read`;
 }
 
-export function dateRange(startDate: Date, endDate?: Date | string): string {
-  const startMonth = startDate.toLocaleString("default", { month: "short" });
-  const startYear = startDate.getFullYear().toString();
-  let endMonth;
-  let endYear;
+export function dateRange(
+  startDate: Date,
+  endDate?: Date | string,
+  { showPeriod = false }: { showPeriod?: boolean } = {}
+): string {
+  const formatDate = (date: Date): string =>
+    `${date.toLocaleString("default", { month: "short" })}${date.getFullYear()}`;
 
-  if (endDate) {
-    if (typeof endDate === "string") {
-      endMonth = "";
-      endYear = endDate;
-    } else {
-      endMonth = endDate.toLocaleString("default", { month: "short" });
-      endYear = endDate.getFullYear().toString();
-    }
+  const calculateExperienceText = (start: Date): string => {
+    const today = new Date();
+    const anniversaryThisYear = new Date(
+      today.getFullYear(),
+      start.getMonth(),
+      start.getDate()
+    );
+
+    const years =
+      today.getFullYear() -
+      start.getFullYear() -
+      (today >= anniversaryThisYear ? 0 : 1);
+
+    const isAnniversaryToday =
+      today.getMonth() === start.getMonth() &&
+      today.getDate() === start.getDate();
+
+    return years < 1 ? "<1" : isAnniversaryToday ? `${years}` : `${years}+`;
+  };
+
+  const startLabel = formatDate(startDate);
+  const endLabel = !endDate
+    ? ""
+    : typeof endDate === "string"
+      ? endDate
+      : formatDate(endDate);
+
+  let result = `${startLabel} - ${endLabel}`;
+
+  if (showPeriod && endDate === "Current") {
+    result += ` (${calculateExperienceText(startDate)} years)`;
   }
 
-  return `${startMonth}${startYear} - ${endMonth}${endYear}`;
+  return result;
 }
